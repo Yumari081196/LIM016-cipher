769a46b71898eb2ecdfbf720cfd82e39ae6a0a6d
unknown <cruzyumari2@gmail.com>
2021-10-06 21:38:14 -0500

    Creamos nuestro objeto cipher y sus metodos

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,38 @@
 const cipher = {
-  // ...
+  encode:(m,y)=>{
+    let newstrinarray="";
+    let x=m%26;
+    for(let i=0;i<y.length;i++){
+      if(parseInt(y.charCodeAt(i))<=90-x && parseInt(y.charCodeAt(i))>=65){
+        newstrinarray+=String.fromCharCode(parseInt(y.charCodeAt(i))+x);
+      }
+      if(parseInt(y.charCodeAt(i))>90-x){
+        newstrinarray+=String.fromCharCode(parseInt(y.charCodeAt(i))-26+x);
+      }
+      if(parseInt(y.charCodeAt(i))==32){
+        newstrinarray+=String.fromCharCode(32);
+      }
+      
+    }
+    return newstrinarray;
+  },
+  decode:(m,y)=>{
+    let newstrinarray="";
+    let x=m%26;
+    for(let i=0;i<y.length;i++){
+      if(parseInt(y.charCodeAt(i))<=90 && parseInt(y.charCodeAt(i))>=65+x){
+        newstrinarray+=String.fromCharCode(parseInt(y.charCodeAt(i))-x);
+      }
+      if(parseInt(y.charCodeAt(i))<65+x && parseInt(y.charCodeAt(i))!=32){
+        newstrinarray+=String.fromCharCode(parseInt(y.charCodeAt(i))+26-x);
+      }
+      if(parseInt(y.charCodeAt(i))==32){
+        newstrinarray+=String.fromCharCode(32);
+      }
+      
+    }
+    return newstrinarray;
+  },
 };
 
 export default cipher;
